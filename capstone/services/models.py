b5450e457a8fdb98d9d02bdf21c0d3a959cb19d3
Michael Rusezera <michaelrusezera@gmail.com>
2025-08-26 20:22:23 +0300

    models for services app

--- capstone/services/models.py
+++ capstone/services/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+from facilities.models import Facility
+
+class Service(models.Model):
+    facility = models.ForeignKey(Facility, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=100, blank=True)
+    skill_type = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.name
