e25af73f6208d7aa11569f1165368327355a6c35
Michael Rusezera <michaelrusezera@gmail.com>
2025-08-26 18:38:43 +0300

    models for facilities app

--- capstone/facilities/models.py
+++ capstone/facilities/models.py
@@ -1,3 +1,13 @@
+
 from django.db import models
 
-# Create your models here.
+class Facility(models.Model):
+    name = models.CharField(max_length=200)
+    location = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    partner_org = models.CharField(max_length=200, blank=True)
+    facility_type = models.CharField(max_length=100, blank=True)
+    capabilities = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
