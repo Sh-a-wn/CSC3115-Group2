e2760d65d4d330d58f8ad83ca0dd01551c334326
Michael Rusezera <michaelrusezera@gmail.com>
2025-08-26 19:43:58 +0300

    managing general routing

--- capstone/capstone/urls.py
+++ capstone/capstone/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('programs/', include('programs.urls')),
+    path('facilities/', include('facilities.urls')),
+    path('services/', include('services.urls')),
+    path('equipment/', include('equipment.urls')),
+    path('projects/', include('projects.urls')),
+    path('participants/', include('participants.urls')),
 ]
