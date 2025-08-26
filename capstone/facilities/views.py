c592539c4041a41a8320128ef2dbc1f8ff699229
Michael Rusezera <michaelrusezera@gmail.com>
2025-08-26 18:50:39 +0300

    views(controller) for facilities app

--- capstone/facilities/views.py
+++ capstone/facilities/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Facility
 
-# Create your views here.
+class FacilityListView(ListView):
+    model = Facility
+
+class FacilityDetailView(DetailView):
+    model = Facility
+
+class FacilityCreateView(CreateView):
+    model = Facility
+    fields = '__all__'
+    success_url = reverse_lazy('facility_list')
+
+class FacilityUpdateView(UpdateView):
+    model = Facility
+    fields = '__all__'
+    success_url = reverse_lazy('facility_list')
+
+class FacilityDeleteView(DeleteView):
+    model = Facility
+    success_url = reverse_lazy('facility_list')
