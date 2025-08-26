e8bbea7102143604ea44cdc1139c8b8e2af5003a
Michael Rusezera <michaelrusezera@gmail.com>
2025-08-26 20:22:53 +0300

    views for services app

--- capstone/services/views.py
+++ capstone/services/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Service
 
-# Create your views here.
+class ServiceListView(ListView):
+    model = Service
+
+class ServiceDetailView(DetailView):
+    model = Service
+
+class ServiceCreateView(CreateView):
+    model = Service
+    fields = '__all__'
+    success_url = reverse_lazy('service_list')
+
+class ServiceUpdateView(UpdateView):
+    model = Service
+    fields = '__all__'
+    success_url = reverse_lazy('service_list')
+
+class ServiceDeleteView(DeleteView):
+    model = Service
+    success_url = reverse_lazy('service_list')
