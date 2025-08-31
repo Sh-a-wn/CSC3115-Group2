9c796de2b23d9712b9f7c7f61e8b55fde4f9ef77
Arnold Okello <okelloarnold4@gmail.com>
2025-08-31 17:17:51 +0300

    views for Outcome management including list, detail, create, update, and delete

---
+++ capstone/outcome/views.py
@@ -0,0 +1,59 @@
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+
+from .models import Outcome
+from projects.models import Project
+
+
+class OutcomeListForProjectView(ListView):
+    template_name = "outcome/outcome_list_for_project.html"
+
+    def get_queryset(self):
+        self.project = get_object_or_404(Project, pk=self.kwargs["project_id"])
+        return Outcome.objects.filter(project=self.project).order_by("-created_at")
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx["project"] = self.project
+        return ctx
+
+
+class OutcomeDetailView(DetailView):
+    model = Outcome
+
+
+class OutcomeCreateView(CreateView):
+    model = Outcome
+    fields = "__all__"
+
+    def get_initial(self):
+        initial = super().get_initial()
+        project_id = self.kwargs.get("project_id")
+        if project_id:
+            initial["project"] = get_object_or_404(Project, pk=project_id)
+        return initial
+
+    def get_success_url(self):
+        return reverse_lazy("project_outcomes", kwargs={"project_id": self.object.project_id})
+
+
+class OutcomeUpdateView(UpdateView):
+    model = Outcome
+    fields = "__all__"
+
+    def get_success_url(self):
+        return reverse_lazy("project_outcomes", kwargs={"project_id": self.object.project_id})
+
+
+class OutcomeDeleteView(DeleteView):
+    model = Outcome
+
+    def get_success_url(self):
+        return reverse_lazy("project_outcomes", kwargs={"project_id": self.object.project_id})
