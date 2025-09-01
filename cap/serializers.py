b1fce373b4e6b7c6b4afe3c4466f9d7c69c6c88b
jescanishacode <jessicanakirijja90@gmail.com>
2025-09-01 12:18:03 +0300

    Added serializers

--- cap/serializers.py
+++ cap/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Equipment, Facility
 
 class EquipmentSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(label="Equipment Name")
+    capabilities = serializers.CharField(label="Key Capabilities")
+    description = serializers.CharField(label="Detailed Description")
+    inventory_code = serializers.CharField(label="Inventory Code")
+    usage_domain = serializers.CharField(label="Domain (Electronics, Mechanical, IoT, etc.)")
+    support_phase = serializers.CharField(label="Support Phase (Training, Prototyping, etc.)")
+    
+    
     class Meta:
         model = Equipment
         fields = '__all__'
